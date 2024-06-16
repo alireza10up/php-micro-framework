8ae316688fcba569651ad8ff5dbe1359999d3439
alireza.vahdani <alireza.vahdani@partsoftware.com>
2024-06-16 09:57:28 +0330

    fix str tok ( router canot detect url with params ) & write doc for request obj

--- App/Core/Request.php
+++ App/Core/Request.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Core;
+namespace App\Core\Http;
 
 use JetBrains\PhpStorm\NoReturn;
 
-class Request
+final class Request
 {
     private array $params;
     private string $method;
@@ -18,50 +18,100 @@ class Request
         $this->agent = $_SERVER['HTTP_USER_AGENT'] ?? '';
         $this->method = strtolower($_SERVER['REQUEST_METHOD'] ?? '');
         $this->ip = $_SERVER['REMOTE_ADDR'] ?? '';
-        $this->uri = $_SERVER['REQUEST_URI'] ?? '';
+        $this->uri = strtok($_SERVER['REQUEST_URI'] ?? '', '?');
     }
 
-    public function __get($name)
+    /**
+     * not exist member handle
+     * search in params and return
+     * 
+     * @param string $name 
+     * @return mixed
+     */
+    public function __get($name): mixed
     {
         return $this->params[$name] ?? null;
     }
 
-    public function params()
+    /**
+     * getter params
+     * 
+     * @return array
+     */
+    public function params(): array
     {
-        return $this->params();
+        return $this->params;
     }
 
+    /**
+     * getter method
+     * 
+     * @return string
+     */
     public function method(): string
     {
         return $this->method;
     }
 
+    /**
+     * getter agent
+     * 
+     * @return string
+     */
     public function agent()
     {
         return $this->agent;
     }
 
+    /**
+     * getter ip
+     * 
+     * @return string
+     */
     public function ip()
     {
         return $this->ip;
     }
 
+    /**
+     * getter url
+     * 
+     * @return string
+     */
     public function uri()
     {
         return $this->uri;
     }
 
+    /**
+     * get special key in params , if not exist replace with default
+     * 
+     * @param string $key 
+     * @param mixed $default
+     */
     public function input($key, $default = null)
     {
         return $this->params[$key] ?? $default;
     }
 
+    /**
+     * check in params has a key
+     * 
+     * @param string $key
+     * @return bool
+     */
     public function has($key): bool
     {
         return isset($this->params[$key]);
     }
 
-    #[NoReturn] public function redirect($route): void
+    /**
+     * redirect and die
+     * 
+     * @param string $route 
+     * @return void
+     */
+    public function redirect($route): void
     {
         header('location:' . site_url($route));
         die();
